dadadf249c942edf32fc5d8f64c04aee89fc580e
AHUYNH00 <AHUYNH00@UW.EDU>
2023-10-10 11:03:28 -0700

    buggy v1

--- src/main/kotlin/edu/uw/basic-kotlin/Homework.kt
+++ src/main/kotlin/edu/uw/basic-kotlin/Homework.kt
@@ -8,14 +8,75 @@ class Library {
     }
 }
 
+fun main () {
+    val pers = Person("me", "he", 45)
+    println(pers.debugString)
+}
+
 // write a "whenFn" that takes an arg of type "Any" and returns a String
+fun whenFn(args: Any): String {
+    val res = when (args) {
+        "Hello" -> "world"
+        is String -> "Say what?" // in assignment write up, it's "I don't understand"
+        0 -> "zero"
+        1 -> "one"
+        in 2..10 -> "low number"
+        is Int -> "a number"
+        else -> "I don't understand"
+    }
+    return res
+}
 
 // write an "add" function that takes two Ints, returns an Int, and adds the values
+fun add(val1: Int, val2: Int): Int {
+    return val1 + val2
+}
 
 // write a "sub" function that takes two Ints, returns an Int, and subtracts the values
+fun sub(val1: Int, val2: Int): Int {
+    return val1 - val2
+}
 
 // write a "mathOp" function that takes two Ints and a function (that takes two Ints and returns an Int), returns an Int, and applies the passed-in-function to the arguments
+fun mathOp(val1: Int, val2:Int, op: (Int, Int) -> Int):Int {
+    return op(val1, val2)
+}
 
 // write a class "Person" with first name, last name and age
+class Person(val firstName: String,
+             val lastName: String,
+             var age: Int,
+             val debugString: String = "[Person firstName:${firstName}, lastName:${lastName}, age:${age}]") {
+                init {
+                    println(debugString)
+                }
+             }
 
 // write a class "Money" with amount and currency, and define a convert() method and the "+" operator
+
+//enum class Currency {
+//  USD, EUR, CAN, GBP
+//}
+
+class Money(val amount: Int, val currency: String) {
+    fun convert(convertTo: String): Money {
+        var conversion = when (currency) {  // convert to USD
+            "GBP" -> amount * 2
+            "EUR" -> amount * (2/3)
+            "CAN" -> amount * (4/5)
+            else -> amount
+        }
+        conversion = when (convertTo) {
+            "GBP" -> conversion * (1/2)
+            "EUR" -> conversion * (3/2)
+            "CAN" -> conversion * (5/4)
+            else -> conversion
+        }
+        return Money(conversion, convertTo)
+    }
+
+    operator fun plus(other: Money):Money {
+        val otherConverted = if (this.currency !== other.currency) other.convert(this.currency) else other
+        return Money(this.amount + otherConverted.amount, this.currency)
+    }
+}

c2888be84cce73ba1e23530b660fa579c23e112b
AHUYNH00 <AHUYNH00@UW.EDU>
2023-10-10 22:55:57 -0700

    debuged

--- src/main/kotlin/edu/uw/basic-kotlin/Homework.kt
+++ src/main/kotlin/edu/uw/basic-kotlin/Homework.kt
@@ -17,7 +17,8 @@ fun main () {
 fun whenFn(args: Any): String {
     val res = when (args) {
         "Hello" -> "world"
-        is String -> "Say what?" // in assignment write up, it's "I don't understand"
+        "Bonjour", "Howdy"-> "Say what?"
+        is String -> "I don't understand"
         0 -> "zero"
         1 -> "one"
         in 2..10 -> "low number"
@@ -46,30 +47,35 @@ fun mathOp(val1: Int, val2:Int, op: (Int, Int) -> Int):Int {
 class Person(val firstName: String,
              val lastName: String,
              var age: Int,
-             val debugString: String = "[Person firstName:${firstName}, lastName:${lastName}, age:${age}]") {
-                init {
-                    println(debugString)
-                }
-             }
+             val debugString: String = "[Person firstName:${firstName} lastName:${lastName} age:${age}]")
+
 
 // write a class "Money" with amount and currency, and define a convert() method and the "+" operator
 
-//enum class Currency {
-//  USD, EUR, CAN, GBP
-//}
+enum class Currency {USD, EUR, CAN, GBP}
 
 class Money(val amount: Int, val currency: String) {
+    init {
+        if (amount < 0) {
+            throw IllegalArgumentException("amount cant be neg")
+        }
+        if (currency !== "USD" && currency !== "EUR" &&
+            currency !== "CAN" && currency !== "GBP") {
+                throw IllegalArgumentException("Currency must be USD/EUR/CAD/GBP")
+        }
+    }
+
     fun convert(convertTo: String): Money {
         var conversion = when (currency) {  // convert to USD
             "GBP" -> amount * 2
-            "EUR" -> amount * (2/3)
-            "CAN" -> amount * (4/5)
+            "EUR" -> amount * 2/3
+            "CAN" -> amount * 4/5
             else -> amount
         }
         conversion = when (convertTo) {
-            "GBP" -> conversion * (1/2)
-            "EUR" -> conversion * (3/2)
-            "CAN" -> conversion * (5/4)
+            "GBP" -> conversion * 1/2
+            "EUR" -> conversion * 3/2
+            "CAN" -> conversion * 5/4
             else -> conversion
         }
         return Money(conversion, convertTo)
